69abf8744299d0f1a88c7be32a7162d3a91561d0
devdl11 <devdl11@proton.me>
2025-04-04 15:32:03 +0200

    add EqualityType class for keyword-based equality parsing

---
+++ src/fr/dl11/openmedia/parsing/EqualityType.java
@@ -0,0 +1,13 @@
+package fr.dl11.openmedia.parsing;
+
+public class EqualityType extends AbsParsable{
+    public static final EqualityType kEqual = new EqualityType("égal à");
+    public static final EqualityType kControl = new EqualityType("contrôle");
+    public static final EqualityType kParticipate = new EqualityType("participe");
+    public static final EqualityType kGreaterThan = new EqualityType("supérieur à");
+    public static final EqualityType kLessThan = new EqualityType("inférieur à");
+
+    EqualityType(String keyword) {
+        super(keyword);
+    }
+}
