3b75e0b79558672a7a85b045c7e3c690dfb850fa
devdl11 <devdl11@proton.me>
2025-04-10 10:59:35 +0200

    refactor: rename AbstractStreamParser to AbstractStreamSegmenter and update method name

--- src/fr/dl11/openmedia/parsing/stream/AbstractStreamSegmenter.java
+++ src/fr/dl11/openmedia/parsing/stream/AbstractStreamSegmenter.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.Reader;
 import java.util.List;
 
-public abstract class AbstractStreamParser {
+public abstract class AbstractStreamSegmenter {
     protected final BufferedReader reader;
 
-    public AbstractStreamParser(Reader reader) {
+    public AbstractStreamSegmenter(Reader reader) {
         this.reader = new BufferedReader(reader);
     }
 
-    public abstract List<StreamFragment> parse() throws Exception;
+    public abstract List<StreamFragment> segment() throws Exception;
 }
