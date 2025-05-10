bb108b40999557438f256c5c353d210df1f66e63
devdl11 <devdl11@proton.me>
2025-05-10 11:06:33 +0200

    feat: make ParsableToken constructors public and update parse method to use toString for token comparison

--- src/fr/dl11/openmedia/parsing/ParsableToken.java
+++ src/fr/dl11/openmedia/parsing/ParsableToken.java
@@ -4,25 +4,24 @@ public class ParsableToken<E extends Enum<E>> implements ITokenParser<E> {
     private final Class<E> enumClass;
     private final E defaultValue;
 
-    ParsableToken(Class<E> enumClass, E defaultValue) {
+    public ParsableToken(Class<E> enumClass, E defaultValue) {
         this.enumClass = enumClass;
         this.defaultValue = defaultValue;
     }
 
-    ParsableToken(Class<E> enumClass) {
+    public ParsableToken(Class<E> enumClass) {
         this(enumClass, null);
     }
 
     public E parse(String token) throws Exception {
         try {
             for (E constant : enumClass.getEnumConstants())
-                if (constant.name().equals(token)) return constant;
+                if (constant.toString().equals(token)) return constant;
         } catch (Exception e) {
             // Handle exception
             e.printStackTrace();
         }
 
-        if (defaultValue != null) return defaultValue;
-        throw new Exception("azeaze");
+        return defaultValue;
     }
 }
