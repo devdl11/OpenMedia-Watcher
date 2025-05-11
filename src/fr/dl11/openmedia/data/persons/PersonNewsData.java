e00979848077ce9cb0e1fee9064aa287726320f5
devdl11 <devdl11@proton.me>
2025-05-11 08:50:05 +0200

    feat: add PersonNewsData class to encapsulate challenge and Forbes rankings with year

---
+++ src/fr/dl11/openmedia/data/persons/PersonNewsData.java
@@ -0,0 +1,51 @@
+package fr.dl11.openmedia.data.persons;
+
+import java.time.Year;
+import java.util.Optional;
+
+public class PersonNewsData {
+    private Integer challengeRank;
+    private Integer forbesRank;
+    private final Year year;
+
+    public PersonNewsData(Integer challengeRank, Integer forbesRank, Year year) {
+        this.challengeRank = challengeRank;
+        this.forbesRank = forbesRank;
+        this.year = year;
+    }
+
+    public PersonNewsData(Year year) {
+        this.challengeRank = null;
+        this.forbesRank = null;
+        this.year = year;
+    }
+
+    public void addChallengeRank(Integer challengeRank) {
+        this.challengeRank = challengeRank;
+    }
+
+    public void addForbesRank(Integer forbesRank) {
+        this.forbesRank = forbesRank;
+    }
+
+    public Optional<Integer> getChallengeRank() {
+        return Optional.ofNullable(challengeRank);
+    }
+
+    public Optional<Integer> getForbesRank() {
+        return Optional.ofNullable(forbesRank);
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonNewsData{" +
+                "challengeRank=" + challengeRank +
+                ", forbesRank=" + forbesRank +
+                ", year=" + year +
+                '}';
+    }
+}
