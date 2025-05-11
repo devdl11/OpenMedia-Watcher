97d1d1e47fa222cbc8bf47cf2678c2cd070a9882
devdl11 <devdl11@proton.me>
2025-05-11 08:50:43 +0200

    feat: add mappers for person and organization data in MediaWatcher

--- src/fr/dl11/openmedia/MediaWatcher.java
+++ src/fr/dl11/openmedia/MediaWatcher.java
@@ -2,7 +2,14 @@ package fr.dl11.openmedia;
 
 import fr.dl11.openmedia.common.events.NewRawDataEvent;
 import fr.dl11.openmedia.data.MediaData;
+import fr.dl11.openmedia.data.mapper.PersonMapper;
+import fr.dl11.openmedia.data.organization.OrganizationData;
+import fr.dl11.openmedia.data.organization.OrganizationMediaLink;
+import fr.dl11.openmedia.data.organization.OrganizationOrganizationLink;
+import fr.dl11.openmedia.data.persons.PersonMediaLinkData;
+import fr.dl11.openmedia.data.persons.PersonOrganizationLink;
 import fr.dl11.openmedia.datasource.DataPipelineManager;
+import fr.dl11.openmedia.datasource.mappers.DataMapperWithIgnores;
 import fr.dl11.openmedia.datasource.mappers.DefaultDataMapper;
 import fr.dl11.openmedia.datasource.parsers.CSVParser;
 import fr.dl11.openmedia.events.EventHandler;
@@ -19,7 +26,18 @@ public class MediaWatcher implements EventHandler<NewRawDataEvent> {
         dataPipelineManager.addParser(DataType.CSV, new CSVParser("\t"));
         // Add other parsers and mappers as needed
 
+        // Add mappers for media data
         dataPipelineManager.addMapper(new DefaultDataMapper<>(MediaData.class));
+
+        // Add mappers for persons
+        dataPipelineManager.addMapper(new PersonMapper());
+        dataPipelineManager.addMapper(new DefaultDataMapper<>(PersonMediaLinkData.class));
+        dataPipelineManager.addMapper(new DataMapperWithIgnores<>(PersonOrganizationLink.class, List.of("id")));
+
+        // Add mappers for organizations
+        dataPipelineManager.addMapper(new DefaultDataMapper<>(OrganizationData.class));
+        dataPipelineManager.addMapper(new DataMapperWithIgnores<>(OrganizationMediaLink.class, List.of("id")));
+        dataPipelineManager.addMapper(new DataMapperWithIgnores<>(OrganizationOrganizationLink.class, List.of("id")));
     }
 
     public MediaWatcher(DataPipelineManager dataPipelineManager) {
